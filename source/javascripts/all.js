df5672f69ac8556dccdd2751e11b877741a6a941
laure lemercinier <laure.lemercinier@gmail.com>
2015-02-10 17:41:33 +0100

    arrows and full height page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,39 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(".window-height").css("height", $(window).height());
+});
+
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "rgba(255, 255, 255, 0.7)",
+        "box-shadow": "0 0 2px grey"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+});
